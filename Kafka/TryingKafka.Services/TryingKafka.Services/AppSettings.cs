6a1e8df282cc14f16e0381ffe99a029ce48d87ac
Dong Zeng <dong@freshbytesoftware.com>
2021-09-05 19:51:29 -0500

    AppSetting classs

---
+++ Kafka/TryingKafka.Services/TryingKafka.Services/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TryingKafka.Services
+{
+    public static class AppSettings
+    {
+        private const string Prefix = "KafkaSettings";
+
+        public static List<KeyValuePair<string, string>> GetConfig(IConfiguration configuration, string key)
+        {
+            return configuration.GetSection($"{Prefix}:{key}").GetChildren()
+                .ToDictionary(x => x.Key, x => x.Value).ToList();
+        }
+
+        public static string GetTopicName(IConfiguration configuration, string key)
+        {
+            return configuration.GetSection($"{Prefix}:Producer:{key}").GetChildren()
+                .ToDictionary(x => x.Key, x => x.Value).ToList()?
+                .Where(x => x.Key.Equals("TopicName")).FirstOrDefault().Value;
+        }
+    }
+}
