ee3536bef225f8801f9435b068b500fa7c08b039
Dong Zeng <dong@freshbytesoftware.com>
2021-09-08 08:31:57 -0500

    Reporting Service

---
+++ Kafka/Services/Services.Reporting/Program.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Text.Json;
+using TryingKafka.KafkaService;
+using TryingKafka.KafkaService.Models;
+
+namespace Services.Reporting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reporting Service");
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true).Build();
+
+            var kafkaService = new KafkaService(configuration);
+
+            while (true)
+            {
+                ConsumeResult<Null, string> subResult;
+                string error;
+
+                (subResult, error) = kafkaService.Subscribe();
+
+                if (error == string.Empty)
+                {
+                    var report = JsonSerializer.Deserialize<Report>(subResult.Message.Value);
+                    Console.WriteLine($"[Report Status: {report.Status}] => [ CreatedOn: {report.CreatedOn}, Report Id: {report.Id}, Order Id: {report.Order.Id}, Report Details: {report.Details} ]");
+                }
+            }
+        }
+    }
+}
