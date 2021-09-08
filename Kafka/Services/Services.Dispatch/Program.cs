05409422c1152466aa827e94c571424378d12048
Dong Zeng <dong@freshbytesoftware.com>
2021-09-08 08:16:07 -0500

    Dispatch service

---
+++ Kafka/Services/Services.Dispatch/Program.cs
@@ -0,0 +1,59 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using TryingKafka.KafkaService;
+using TryingKafka.KafkaService.Models;
+
+namespace Services.Dispatch
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Dispatch Service");
+
+            ConsumeResult<Null, string> subResult;
+            DeliveryResult<Null, string> pubResult;
+            string error;
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true).Build();
+
+            var producerReportedTopicName = AppSettings.GetTopicName(configuration, "Reported");
+
+            var kafkaService = new KafkaService(configuration);
+
+            while (true)
+            {
+                (subResult, error) = kafkaService.Subscribe();
+
+                if (error == string.Empty)
+                {
+                    var order = JsonSerializer.Deserialize<Order>(subResult.Message.Value);
+
+                    var report = DoDispatch(order);
+
+                    string jsonData = JsonSerializer.Serialize(report);
+                    (pubResult, error) = await kafkaService.Publish(producerReportedTopicName, jsonData);
+                }
+            }
+        }
+
+        private static Report DoDispatch(Order order)
+        {
+            Thread.Sleep(1000);
+
+            return new Report()
+            {
+                Id = Guid.NewGuid(),
+                Order = order,
+                Details = "Order has been dispatched",
+                Status = Status.OrderDispatched,
+                CreatedOn = DateTime.Now
+            };
+        }
+    }
+}
